978199fc8cc8b6031b89d591945be0eea550700b
NicolasfBrandini <nicolasfbrandini@hotmail.com>
2023-10-29 17:11:06 -0300

    ajustes

--- src/Entrada.js
+++ src/Entrada.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Entrada.css";
+import "tailwindcss/base.css";
+import "tailwindcss/components.css";
+import "tailwindcss/utilities.css";
 
 const Entrada = () => {
   return (
     <body className="body">
-      <div>
-        <h1 className="titulo">
-          Clique em continuar para ter acesso ao pokedex!
-        </h1>
-        <button variant="contained" color="primary" className="button button1">
-          <Link to="/pokedex">Continuar</Link>
-        </button>
+      <div className="h-screen flex flex-col items-center justify-between">
+        <section className="text-center">
+          <h1 className="titulo font-bold py-2 px-4 pt-10 rounded text-3xl">
+            Bem vindo ao Pokedex!
+          </h1>
+        </section>
+        <div className=" pb-20">
+          <button className=" btn text-white font-bold py-2 px-4 rounded">
+            <Link to="/pokedex">Continuar</Link>
+            <span className="btn-1"></span>
+            <span className="btn-2"></span>
+            <span className="btn-3"></span>
+            <span className="btn-4"></span>
+          </button>
+        </div>
       </div>
     </body>
   );
